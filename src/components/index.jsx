1771a3f63fd2c664bf781541d6c9d8d10b3a7eda
crudeboy <jahswill.apata@decagon.dev>
2022-02-24 11:04:09 +0100

    change letter casing

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,3 @@
 export { default as Cards } from './Cards/Cards';
 export { default as Charts } from './Charts/Charts';
-export { default as CountryPicker } from './CountryPicker/CountryPicker';
+export { default as CountryPicker } from './CountryPicker/untryPicker';
